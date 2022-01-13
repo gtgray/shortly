96dd2bc12a807f23df4ac31459993992574e469f
gtgray <gtgray33@gmail.com>
2022-01-13 10:05:22 +0300

    Broke static code analysis rules

--- app/src/main/java/tk/atna/shortlyapp/App.kt
+++ app/src/main/java/tk/atna/shortlyapp/App.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import tk.atna.shortlyapp.di.appModule
 
+
 class App : Application() {
 
     override fun onCreate() {
